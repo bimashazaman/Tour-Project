70dbaf8c8093126eeb3a6019491337d4ff19ecab
bimashazaman <bimashazaman1234@gmail.com>
2022-05-02 01:31:43 +0600

    first commit

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = ({tours}) => {
+  return (
+    <section>
+      <h2>Tours</h2>
+      <div className='underLine'>
+        {tours.map(tour => (
+          <Tour key={tour.id} {...tour} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
